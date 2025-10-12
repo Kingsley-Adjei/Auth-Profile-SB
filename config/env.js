1181bc13d552696bae7111f40b63d0895efae643
YUNGZEUS <kingkadjei002@email.com>
2025-10-12 01:02:33 +0000

    chore: added the environment for AI service

--- config/env.js
+++ config/env.js
@@ -2,4 +2,5 @@ import { config } from "dotenv";
 
 config();
 
-export const { PORT, NODE_ENV, SUPABASE_URL, SUPABASE_KEY } = process.env;
+export const { PORT, NODE_ENV, SUPABASE_URL, SUPABASE_KEY, AI_SERVICE_URL } =
+  process.env;

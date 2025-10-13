21bada07e89077484ae59c06e53dde4b48e839c2
YUNGZEUS <kingkadjei002@email.com>
2025-10-13 06:16:05 +0000

    (fix:ai) improved aiService file with error logs

--- service/aiService.js
+++ service/aiService.js
@@ -4,7 +4,12 @@ import { AI_SERVICE_URL } from "../config/env";
 const AI_URL = AI_SERVICE_URL;
 
 export async function getMatchFromAI(userSkills, taskRequirements) {
-  const payload = { userSkills, taskRequirements };
-  const response = await axios.post(AI_URL, payload, { timeout: 10000 });
-  return response.data;
+  try {
+    const payload = { userSkills, taskRequirements };
+    const response = await axios.post(AI_URL, payload, { timeout: 10000 });
+    return response.data;
+  } catch (error) {
+    console.error("Error calling Ai service", error.message);
+    throw new Error("Failed to get AI match result");
+  }
 }
